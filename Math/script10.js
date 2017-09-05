5ddafc76c9f84731fb72b04870f65d635ab2212c
Martyniuk <VVMartyniuk@kiev.luxoft.com>
2017-09-05 18:05:02 +0300

    9,10

---
+++ Math/script10.js
@@ -0,0 +1,34 @@
+//Write a JavaScript function to get the least common multiple (LCM) of two numbers.
+
+function lcm(a, b) {
+    let arr = [];
+
+    for (var i = a; i <= b; i++) {
+        arr.push(i);
+    }
+
+    console.log(arr);
+}
+
+
+
+/* function lcm(a, b) {
+    let firstArr = [],
+        secondArr = [];
+    for (var i = 1; i <= 10; i++) {
+        firstArr.push(a * i);
+    }
+    for (var i = 1; i <= 10; i++) {
+        secondArr.push(b * i);
+    }
+    let common = firstArr.concat(secondArr).sort((a, b) => a > b ? 1 : -1);
+    console.log(common);
+
+    return common.reduceRight((reslt, i) => {
+        if (i % a == 0 && i % b == 0) {
+            return i;
+        }
+    }, common[0]);
+} */
+
+console.log(lcm(10, 15));
--- Math/script9.js
+++ Math/script9.js
@@ -1,1 +1,17 @@
 //Write a JavaScript function to find the GCD (greatest common divisor) of more than 2 integers.
+
+function GCD(arr) {
+    if (toString().call(arr) !== "[object Array]") {
+        return false;
+    }
+    var res = 0;
+    arr.reduce((c, i) => {       
+        while(c !== i) {
+            c > i ? c = c - i : i = i - c;
+        }
+       return res = c;
+    }, arr[0]);
+    return res;
+}
+
+console.log(GCD([3, 15, 27]));
