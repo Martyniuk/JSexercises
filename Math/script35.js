35f3873da50f34c96f6f99a2c1bd8885f5eb89dd
Vladimir <vladimir@bestMacEver.local>
2017-09-27 23:15:33 +0300

    same story...

---
+++ Math/script35.js
@@ -0,0 +1,5 @@
+// Write a JavaScript function for the Pythagorean theorem.
+
+function pythagoreanTheorem(a, b) {
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
---
+++ Math/script36.js
@@ -0,0 +1,5 @@
+//Write a JavaScript function which will return values that are powers of two.
+
+function isPowerOfTwo(number) {
+    return Math.sqrt(number) % 2 === 0;
+}
